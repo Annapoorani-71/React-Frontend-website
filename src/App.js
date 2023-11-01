e365aef2e96cfb3ca8027f43ce51e477d546d071
Annapoorani-71 <annapooranikumar71@gmail.com>
2023-11-01 07:02:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import Footer from './components/Footer';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import { useState } from 'react';
 
 function App() {
+  
+   
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  const backgroundColor = theme === 'light' ? '#fff' : '#333';
+  const textColor = theme === 'light' ? '#333' : '#fff';
+
+  const theme1 = {
+    backgroundColor: 'blue',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ backgroundColor, color: textColor }}>
+      <Header />
+      <div className="spacer">
+        <div className="button-container">
+          <button
+            className="toggle-button"
+            onClick={toggleTheme}
+            style={{ backgroundColor, color: textColor }}
+          >
+            Toggle Theme
+          </button>
+        </div>
+      </div>
+      <Footer/>
+     </div>
   );
 }
 
